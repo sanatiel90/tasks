444f8e35da5de68a0fa2254118156d1e77eb9371
sanatiel <sanatielbarros90@gmail.com>
2020-01-03 16:28:23 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
 import './database'
 
 class App {
@@ -12,11 +13,19 @@ class App {
 
     middlewares() {
         this.server.use(express.json())
+        
+        const corsOptions = {
+            origin: 'http://localhost:3000',
+            optionsSuccessStatus: 200
+        }
+        
+        this.server.use(cors(corsOptions))
     }
 
     routes() {
         this.server.use(routes)
     }
+    
 }
 
 export default new App().server
